6bfcb972f1e2d5260d5657df294e407e1461d5fe
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-08 01:26:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end               
 
 def long_planeteer_calls(calls)
-  calls.any? {|calls|calls.length >=4} 
+  calls.any? {|calls|calls.length >=4} #=false
   
   
 end

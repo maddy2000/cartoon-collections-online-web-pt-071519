78ecb8687dc52dbd43f6fb748e9f0cebdfbcce1a
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-08 01:38:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,7 @@ def summon_captain_planet(array)
 end               
 
 def long_planeteer_calls(calls)
-  calls.any? {|calls|calls.length >=4}
-   return true
+  calls.any? {|call|call.length >=4}
 end
 
 def find_the_cheese# code an argument here

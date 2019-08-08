52bc416d94b86125f64933a79ecd295740e74c8e
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-08 03:15:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,31 +1,26 @@
+
 require 'pry'
 def roll_call_dwarves(dwarves)
  dwarves.each_with_index do |dwarves, index|
   puts "#{index+1}. #{dwarves}"
 end
 end
-An error occurred while loading ./spec/cartoon_collections_spec.rb.
-Failure/Error: require_relative "../cartoon_collections"
 
-SyntaxError:
-  /home/madelyn/cartoon-collections-online-web-pt-071519/cartoon_collections.rb:9: syntax error, unexpected '.'
-    array.map! {|x|.capitalize+ "!"}
-                   ^
-# ./spec/spec_helper.rb:1:in `require_relative'
-# ./spec/spec_helper.rb:1:in `<top (required)>'
-# ./spec/cartoon_collections_spec.rb:1:in `<top (required)>'
-No examples found.
+def summon_captain_planet(array)
+    array.map {|x|x.capitalize + "!"}
+end               
 
-Finished in 0.00051 seconds (files took 0.1599 seconds to load)
-0 examples, 0 failures, 1 error occurred outside of examples
-
-[13:10:44] (master) cartoon-collections-online-web-pt-071519
-// ♥
 def long_planeteer_calls(calls)
-   calls.length 
+  calls.any? {|call|call.length >4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
+  foods.each do |food|
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if cheese_types.include?(food)
+    return food
+  end
+  
+  end
+nil
 end

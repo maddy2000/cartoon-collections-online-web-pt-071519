e0c9835d820772400fc50b198fa41c33faf54070
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-08 01:55:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def long_planeteer_calls(calls)
   calls.any? {|call|call.length >4}
 end
 
-def find_the_cheese(mouse)
-  mouse.include? ("0")
+def find_the_cheese(cheese_types)
+  cheese_types.include? ("")
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
